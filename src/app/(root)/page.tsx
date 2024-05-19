b738d9d6a4fae292f96e506e95036ddbfe6666ed
Lenox <lenoxmiheso@gmail.com>
2024-05-19 23:45:29 +0300

    Add a HeaderBox and ToolBalanceBox to the root Home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import HeaderBox from "@/components/globals/headerBox"
+import TotalBalanceBox from "@/components/root/TotalBalanceBox"
+
 type Props = {}
 
 const Home = (props: Props) => {
+  const loggedIn = {firstName: 'Lenox'}
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
